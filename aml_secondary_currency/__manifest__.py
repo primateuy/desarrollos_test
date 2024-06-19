5d9440eccf50702bb92c77263eeac37c458a084b
MatiasMastrangelo <mrmastra@hotmail.com>
2024-06-19 09:23:39 -0300

    cambio campo inverserate por inverse_rate

--- aml_secondary_currency/__manifest__.py
+++ aml_secondary_currency/__manifest__.py
@@ -16,7 +16,7 @@
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'account',
-    'version': '16.0.0',
+    'version': '16.0.1',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'account'],
