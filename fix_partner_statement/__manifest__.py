a403d23b06df12a6a68a0fa997d6e24aac05065b
MatiasMastrangelo <mrmastra@hotmail.com>
2024-07-05 00:01:54 -0300

    fix partner statement

---
+++ fix_partner_statement/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Fix Partner Statement",
+    "version": "16.0.0",
+    "category": "Accounting & Finance",
+    "summary": "Fix for OCA Financial Reports (Filter exchange differences)",
+    "author": "PrimateUY",
+    "website": "https://www.primate.uy/",
+    "license": "AGPL-3",
+    "depends": ["partner_statement"],
+    "data": [
+    ],
+    "installable": True,
+    "application": False,
+}
