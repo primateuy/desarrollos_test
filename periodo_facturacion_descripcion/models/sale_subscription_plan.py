2d9af9be28f9e3937b8373c3779bbba4c109dc35
MallicTesla <el_mallic@hotmail.com>
2025-04-30 10:58:32 -0300

    se agrego una nueva funsionalidad al modulo periodo_facturacion_descripcion para eliminar la fecha de inicio y finalizacion de la factura

--- periodo_facturacion_descripcion/models/sale_subscription_plan.py
+++ periodo_facturacion_descripcion/models/sale_subscription_plan.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from odoo import models, fields
 
 class SaleSubscriptionPlan(models.Model):
@@ -7,4 +8,10 @@ class SaleSubscriptionPlan(models.Model):
         string="Usar descripción de cotización",
         default=True,
         help="Si se activa, la factura usará únicamente la descripción original de la cotización sin concatenar datos de la recurrencia."
+    )
+
+    descartar_devengamiento = fields.Boolean(
+        string="Descartar devengamiento",
+        default=False,
+        help="Si se activa, al generar facturas en borrador se dejarán en blanco las fechas desde y hasta del renglón de factura."
     )
