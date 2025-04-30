ca2a8f5c73fb470199cb296eccfbe5313679dd9a
aiglesas <aiglesias.giordano@gmail.com>
2025-04-30 15:40:17 -0300

    Add module Control de Elimincación de Pagos

---
+++ control_eliminacion_de_pagos/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Control de Eliminación de Pagos",
+    "version": "17.0.1.0.0",
+    "category": "Accounting",
+    "summary": "Permite eliminar pagos solo a ciertos grupos de usuarios",
+    "author": "Tu Nombre o Empresa",
+    "depends": ["account"],
+    "data": [
+        "security/delete_payment_groups.xml",
+        'security/ir.model.access.csv',
+        "views/account_payment_view.xml",
+    ],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+}
---
+++ control_eliminacion_de_pagos/models/account_payment.py
@@ -0,0 +1,10 @@
+from odoo import models, _
+from odoo.exceptions import UserError
+
+class AccountPayment(models.Model):
+    _inherit = 'account.payment'
+
+    def unlink(self):
+        if not self.env.user.has_group('control_eliminacion_de_pagos.group_delete_payment'):
+            raise UserError(_("No tiene permisos para eliminar pagos."))
+        return super().unlink()
