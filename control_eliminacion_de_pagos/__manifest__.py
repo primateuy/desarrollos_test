727ab3defb2bb19a43dcfc47ce58cc476fac7f1e
aiglesas <aiglesias.giordano@gmail.com>
2025-05-02 11:40:37 -0300

    Add name to manifest

--- control_eliminacion_de_pagos/__manifest__.py
+++ control_eliminacion_de_pagos/__manifest__.py
@@ -3,7 +3,9 @@
     "version": "17.0.1.0.0",
     "category": "Accounting",
     "summary": "Permite eliminar pagos solo a ciertos grupos de usuarios",
-    "author": "Tu Nombre o Empresa",
+    "author": "Andrés Iglesias / PrimateUy",
+    "website": "https://www.primate.uy",
+    "license": "AGPL-3",
     "depends": ["account"],
     "data": [
         "security/delete_payment_groups.xml",
