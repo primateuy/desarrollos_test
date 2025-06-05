9c29f544f40f68eba7975ffa31ae58baedff8414
dyturralde <darylyturraldelopez@gmail.com>
2025-06-05 17:24:43 -0300

    [ADD] Agregando boolean en Suscripcion para Cancelar excepción de Contrato

--- periodo_facturacion_descripcion/models/sale_order.py
+++ periodo_facturacion_descripcion/models/sale_order.py
@@ -1,15 +1,19 @@
 # -*- coding: utf-8 -*-
 import logging
-from odoo import models, api
+from odoo import models, api, fields
 
 _logger = logging.getLogger(__name__)
 
 class AccountMove(models.Model):
     _inherit = 'sale.order'
 
+    is_payment_exception_cancel = fields.Boolean(
+        string="Cancelar excepción de Contrato",
+        default=False)
+
     def _create_recurring_invoice(self, batch_size=30):
         account_moves = super()._create_recurring_invoice(batch_size)
-        all_subscriptions = self.env['sale.order'].search([('payment_exception', '=', True)])
+        all_subscriptions = self.env['sale.order'].search([('is_payment_exception_cancel', '=', True)])
         for subscription in all_subscriptions:
             if subscription.payment_exception:
                 subscription.payment_exception = False
