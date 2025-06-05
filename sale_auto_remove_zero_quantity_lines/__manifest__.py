cfb38f649b941a9ba54e005432e068d402d09a9c
primateuy <diego@primate.uy>
2025-06-05 15:19:03 -0300

    zero remove

---
+++ sale_auto_remove_zero_quantity_lines/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Sale Auto Remove Zero Quantity Lines",
+    "version": "17.0.1.0.0",
+    "author": "Camptocamp, Odoo Community Association (OCA)",
+    "maintainers": ["camptocamp"],
+    "summary": "On sale confirmation remove lines with zero quantities",
+    "website": "https://github.com/OCA/sale-workflow",
+    "license": "AGPL-3",
+    "depends": ["sale"],
+    "category": "Sales/Sales",
+    "data": [
+        "views/res_config_settings_views.xml",
+    ],
+    "installable": True,
+    "application": False,
+    "development_status": "Beta",
+}
---
+++ sale_auto_remove_zero_quantity_lines/models/res_company.py
@@ -0,0 +1,13 @@
+# Copyright 2024 Camptocamp (<https://www.camptocamp.com>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import fields, models
+
+
+class ResCompany(models.Model):
+    _inherit = "res.company"
+
+    sale_auto_remove_zero_quantity_lines = fields.Boolean(
+        string="Eliminar líneas de venta con cantidad cero",
+        help="Elimina automáticamente las líneas del pedido de venta que tengan cantidad cero al confirmar el pedido.",
+    )
