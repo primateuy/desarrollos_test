bb16e026d96525d92209ad98a24ace9cf0d5a624
aiglesas <aiglesias.giordano@gmail.com>
2025-06-20 17:02:07 -0300

    README.md

--- stock_report/__manifest__.py
+++ stock_report/__manifest__.py
@@ -5,12 +5,14 @@
     "author": "Andrés Iglesias / Primate Uy",
     "category": "Inventory",
     "data": [
-        "security/ir.model.access.csv",
         "security/stock_report_security.xml",
+        "security/ir.model.access.csv",
         "views/res_users_view.xml",
         "views/stock_report_view.xml",
         "views/stock_warehouse_views.xml"
     ],
+    "post_init_hook": "post_init_assign_default_warehouse_group",
     "installable": True,
     "application": False,
+
 }
--- stock_report/models/__init__.py
+++ stock_report/models/__init__.py
@@ -1,2 +1,6 @@
 # -*- coding: utf-8 -*-
 from . import stock_warehouse
+from . import stock_report
+from . import res_users
+from . import stock_warehouse_group_report
+
